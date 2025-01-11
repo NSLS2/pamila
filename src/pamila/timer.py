5426bbe78204b79d9af2504fbb00d5141c97073f
Yoshiteru Hidaka <yhidaka@bnl.gov>
2025-01-11 12:49:53 -0500

    Timer/TimerDict improvements
    - Nested context manager possible
    - Also return lists of strings for saving to text files

--- src/pamila/timer.py
+++ src/pamila/timer.py
@@ -28,9 +28,12 @@ class Timer:
     def total_seconds(self):
         return self.raw_dt
 
-    def print(self, decimal: int = 1):
+    def get_print_str(self, decimal: int = 3):
         dt = self.raw_dt
-        print(f"Elapsed ({self.name}) = {dt:.{decimal}f} [s]")
+        return f"Elapsed ({self.name}) = {dt:.{decimal}f} [s]"
+
+    def print(self, decimal: int = 3):
+        print(self.get_print_str(decimal=decimal))
 
     def get_start_time_str(self):
         return self.start_wall_clock.strftime("%Y%m%dT%H%M%S.%f")
@@ -42,6 +45,9 @@ class Timer:
 
 
 class TimerDict(dict):
+    _timeit_names = []
+    _timeit_kwargs = {}
+
     def start(self, name, exist_ok: bool = True):
         if (not exist_ok) and (name in self):
             raise ValueError(
@@ -51,25 +57,31 @@ class TimerDict(dict):
         self[name] = timer
         return self
 
+    def get_print_lines(self, decimal: int = 3):
+        return [v.get_print_str(decimal=decimal) for v in self.values()]
+
     def print(self, decimal: int = 3):
-        for v in self.values():
-            v.print(decimal=decimal)
+        print("\n".join(self.get_print_lines(decimal=decimal)))
 
     def __enter__(self):
-        if "name" not in self._timeit_kwargs:
+        if len(self._timeit_names) == 0:
             raise RuntimeError(
                 "You must use context like 'with TimerDict().timeit(label):'."
             )
-        self.start(
-            self._timeit_kwargs["name"], exist_ok=self._timeit_kwargs["exist_ok"]
-        )
+        name = self._timeit_names[-1]
+        kw = self._timeit_kwargs[name]
+        self.start(name, exist_ok=kw["exist_ok"])
+
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
+        name = self._timeit_names[-1]
+        self[name].stop()
 
-        self[self._timeit_kwargs["name"]].stop()
-        self._timeit_kwargs.clear()
+        self._timeit_names.pop()
+        del self._timeit_kwargs[name]
 
     def timeit(self, name, exist_ok: bool = True):
-        self._timeit_kwargs = dict(name=name, exist_ok=exist_ok)
+        self._timeit_names.append(name)
+        self._timeit_kwargs[name] = dict(exist_ok=exist_ok)
         return self
